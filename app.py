bac8aca79b779eb6d3befd0f33198fa69c71f08b
GsTalks <garg.nityansh21@gmail.com>
2020-12-03 00:24:17 +0530

    sprint 2 added user login/logout/register

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect,flash, make_response
 from flask_bootstrap import Bootstrap
-from db_query import searchGame
+import db_query
 from flask_cors import CORS
 import json
 
@@ -8,26 +8,129 @@ import json
 app = Flask(__name__)
 Bootstrap(app)
 CORS(app)
+app.secret_key = "super secret key"
 
 @app.route('/')
+@app.route('/index.html')
 def index():
-	games = searchGame("%")
-	print(games)
-	print("\n this is going", jsonify(games))
-	return render_template('index.html', games=games)
-	#return jsonify(games)
+	username = request.cookies.get("username")
+	games_status, games_list = db_query.searchGame("%")
+	if games_status == 200:
+		return render_template('index.html', games=games_list,username=username)
+	else:
+		flash(games_list)
+		return render_template('index.html',games=0,username=username)
+
 
 @app.route('/search',  methods=['GET'])
 def search():
+	username = request.cookies.get("username")
 	print("hello mr. how do you do",request.args.get('search'))
 	if not request.args.get('search'):
-		return "invalid search"
-	game_list = searchGame(request.args.get('search'))
-	if game_list == None:
-		return "No such game is found"
-	print("in the app.py : ",game_list)
-	return render_template('index.html', games=game_list)
-	#return jsonify(game_list)
+		flash("Please provide a correct vlaue")
+		return redirect("/")
+	game_status, game_list = db_query.searchGame(request.args.get('search'))
+	if game_status == 500:
+		return redirect("/")
+	elif game_status==404:
+		flash(game_list)
+		return redirect("index.html")
+	elif game_list == None:
+		return redirect("/")
+	return render_template('index.html', games=game_list,username=username)
+
+
+@app.route('/login.html')
+def login():
+	return render_template('login.html')
+
+@app.route('/signup.html')
+def signup():
+	return render_template('signup.html')
+
+@app.route('/photo-detail.html')
+def photodetail():
+	username = request.cookies.get("username")
+	return render_template('photo-detail.html',username=username)
+
+@app.route('/about.html')
+def about():
+	username = request.cookies.get("username")
+	return render_template("about.html",username=username)
+
+@app.route('/contact.html')
+def contact():
+	username = request.cookies.get("username")
+	return render_template('contact.html',username=username)
+
+@app.route('/register', methods=['POST'])
+def register():
+	#method to register user into database
+	username = request.form.get('username')
+	fname = request.form.get('fname')
+	lname = request.form.get('lname')
+	phno = request.form.get('phno')
+	emailid = request.form.get('emailid')
+	password = request.form.get('password')
+	usertype = request.form.get('usertype')
+	if usertype=="Gamer":
+		usertype = False
+	else:
+		usertype = True
+	if username==None or fname==None or lname == None or phno ==None or emailid==None or password==None:
+		flash("incorrect input")
+		return render_template('signup.html')
+	else:
+		user = {"username":username,"fname":fname, "lname":lname, "phno":phno, "emailid":emailid, "password":password, "usertype":usertype}
+		print(user)
+		registeration,error = db_query.registerUser(user)
+		if registeration==500:
+			flash(error)
+			return render_template('signup.html')
+		elif registeration==200:
+			resp = make_response(render_template('index.html',username=username))
+			resp.set_cookie("username",username)
+			return resp
+
+@app.route('/signin', methods=['POST'])
+def signin():
+	emailid = request.form.get('emailid')
+	password = request.form.get('password')
+	if emailid==None or password==None:
+		return render_template('login.html')
+	else:
+		user = {'emailid':emailid, "password":password}
+		check_status, error = db_query.checkUser(user)
+		print(user)
+		if check_status==500:
+			flash(error)
+			return render_template('login.html')
+		elif check_status==404:
+			flash("invalid login credentials")
+			return render_template('login.html')
+		elif check_status==200:
+			resp = make_response(render_template("index.html",username=error))
+			resp.set_cookie("username", error)
+			return resp
+
+
+@app.route('/game', methods=["GET","POST"])
+def game():
+	username = request.cookies.get("username")
+	gameName = request.args.get("type")
+	print(gameName)
+	game_desc_status, game_desc = db_query.searchGame(gameName)
+	print(game_desc)
+	if game_desc_status==200:
+		return render_template("photo-detail.html",game_desc = game_desc,username=username)
+	else:
+		return redirect("/")
+
+@app.route('/logout')
+def logout():
+	resp = make_response(redirect('/'))
+	resp.delete_cookie('username')
+	return resp
 
 if __name__ == "__main__":
 	app.run(debug=True)
--- db_query.py
+++ db_query.py
@@ -4,7 +4,7 @@ import json
 def searchGame(game_name):
 	try:
 		connection = psycopg2.connect(user = "postgres",
-	                                  password = "********",
+	                                  password = "Garg@9406608047",
 	                                  host = "127.0.0.1",
 	                                  port = "5432",
 	                                  database = "game_selling_marketplace")
@@ -12,20 +12,64 @@ def searchGame(game_name):
 		# Print PostgreSQL Connection properties
 		#print ( connection.get_dsn_parameters(),"\n")
 		# Print PostgreSQL version
-		cursor.execute("SELECT game_name FROM identty where game_name like '%"+ game_name + "%'")
+		cursor.execute("SELECT * FROM game where game_name like '%"+ game_name + "%'")
 		record = cursor.fetchall()
 		#print (record,"\n")
-
-	except (Exception, psycopg2.Error) as error :
-		print ("Error while connecting to PostgreSQL", error)
-	finally:
-		print("is in final")
 		#closing database connection.
 		if(connection):
 			cursor.close()
 			connection.close()
+
+		if len(record)!=0:
+			return 200,record
+		return 404, "No Such Games"
+
+	except (Exception, psycopg2.Error) as error :
+		print ("Error while connecting to PostgreSQL", error)
+		return 500,error
+	
+		
+#print(searchGame("%"))
+
+def registerUser(user):
+	try:
+		connection = psycopg2.connect(user = "postgres",
+	                                  password = "Garg@9406608047",
+	                                  host = "127.0.0.1",
+	                                  port = "5432",
+	                                  database = "game_selling_marketplace")
+		cursor = connection.cursor()
+		cursor.execute("INSERT INTO user_details (username,fname,lname,phno,emailid,password,usertype) Values (%s,%s,%s,%s,%s,crypt(%s,gen_salt('bf')),%s)", (user['username'],user['fname'],user['lname'],str(user['phno']),user['emailid'],user['password'],user['usertype']))
+		print("is in final")
+		#closing database connection.
+		if(connection):
+			connection.commit()
+			cursor.close()
+			connection.close()
 			print("PostgreSQL connection is closed")
 		
-		return record
+		return 200, "OK"
+	except (Exception, psycopg2.Error) as error :
+		print ("Error while connecting to PostgreSQL", error)
+		return 500,error
 
-#print(searchGame("%"))
+def checkUser(user):
+	try:
+		connection = psycopg2.connect(user = "postgres",
+	                                  password = "Garg@9406608047",
+	                                  host = "127.0.0.1",
+	                                  port = "5432",
+	                                  database = "game_selling_marketplace")
+		cursor = connection.cursor()
+		cursor.execute("SELECT username,emailid,password FROM user_details WHERE emailid=%s AND password = crypt(%s, password)", (user['emailid'],user['password']))
+		record = cursor.fetchall()
+		#closing database connection.
+		if(connection):
+			cursor.close()
+			connection.close()
+		if len(record) == 0:
+			return 404, "empty"
+		return 200, record[0][0]
+	except (Exception, psycopg2.Error) as error :
+		print ("Error while connecting to PostgreSQL", error)
+		return 500,error
